7ca329a98b08f18a45fbcabce76ed4b077acac26
Gian <gianseveriche97@gmail.com>
2025-10-16 23:16:01 -0500

    se añadio nueva funcion

--- mis entornos/mi_web/src/proyecto/base/urls.py
+++ mis entornos/mi_web/src/proyecto/base/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
-from .views import lista_pendientes
+from .views import lista_pendientes, DetalleTarea
 
-urlpatterns = [path("", lista_pendientes.as_view(), name="pendientes")]
+urlpatterns = [path("", lista_pendientes.as_view(), name="pendientes"),
+               path("tarea/<int:pk", DetalleTarea.as_view(), name="tarea")]
