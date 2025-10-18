4c9363a58ad21b5672e58fa3c89fab212f78c568
Gian <gianseveriche97@gmail.com>
2025-10-17 21:54:24 -0500

    cambio minimo

--- mis entornos/mi_web/src/proyecto/base/urls.py
+++ mis entornos/mi_web/src/proyecto/base/urls.py
@@ -3,4 +3,4 @@ from .views import lista_pendientes, DetalleTarea, CrearTarea
 
 urlpatterns = [path("", lista_pendientes.as_view(), name="pendientes"),
                path("tarea/<int:pk", DetalleTarea.as_view(), name="tarea"),
-               path("crear_tarea/", CrearTarea.as_view(), name="Crear_tarea")]
+               path("crear_tarea/", CrearTarea.as_view(), name="crear_tarea")]
