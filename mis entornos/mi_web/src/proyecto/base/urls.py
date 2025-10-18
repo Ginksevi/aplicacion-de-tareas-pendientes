f30a57ba8d66ebdedf70ec95edbefc93c4782275
Gian <gianseveriche97@gmail.com>
2025-10-17 21:43:20 -0500

    añadi una nueva ruta para la pagina de creacion de tareas en la pagina principal

--- mis entornos/mi_web/src/proyecto/base/urls.py
+++ mis entornos/mi_web/src/proyecto/base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import lista_pendientes, DetalleTarea
+from .views import lista_pendientes, DetalleTarea, CrearTarea
 
 urlpatterns = [path("", lista_pendientes.as_view(), name="pendientes"),
-               path("tarea/<int:pk", DetalleTarea.as_view(), name="tarea")]
+               path("tarea/<int:pk", DetalleTarea.as_view(), name="tarea"),
+               path("crear_tarea/", CrearTarea.as_view(), name="Crear_tarea")]
