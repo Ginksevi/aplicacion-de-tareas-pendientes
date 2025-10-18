ef26c1725348d1269621842152f3fff20bf3d5db
Gian <gianseveriche97@gmail.com>
2025-10-17 22:20:42 -0500

    agreue una nueva clase y una nueva ruta para eliminar tareas ya creadas

--- mis entornos/mi_web/src/proyecto/base/urls.py
+++ mis entornos/mi_web/src/proyecto/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import lista_pendientes, DetalleTarea, CrearTarea, EditarTarea
+from .views import lista_pendientes, DetalleTarea, CrearTarea, EditarTarea,EliminarTarea
 
 urlpatterns = [path("", lista_pendientes.as_view(), name="pendientes"),
                path("tarea/<int:pk", DetalleTarea.as_view(), name="tarea"),
                path("crear_tarea/", CrearTarea.as_view(), name="crear_tarea"),
-               path("editar_tarea/<int:pk", EditarTarea.as_view(), name="editar_tarea")]
+               path("editar_tarea/<int:pk", EditarTarea.as_view(), name="editar_tarea"),
+               path("eliminar_tarea/<int:pk", EliminarTarea.as_view(), name="eliminar_tarea")]
